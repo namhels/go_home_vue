ae4533d3b50b0d4dbafa925e91b952f2161d7d30
Serhii Lekhman <namhels@MacBook-Pro-Serhii.local>
2023-11-15 08:53:09 +0100

    test

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  pages: {
+    index: {
+      // entry for the page
+      entry: "src/main.js",
+      title: "go_home",
+    },
+  },
+
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "go_home";
+      return args;
+    });
+  },
+});
