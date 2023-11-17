721dd1f7447206767e0e2a443df4c1a818685eb3
Serhii Lekhman <8.lsv.888@gmail.com>
2023-11-17 11:22:54 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@ export default {
 
 <style module>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
